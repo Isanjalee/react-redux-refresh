1a3a1781a52db2973a1bc57bcee4e936c4bbdad0
Isanjalee <ihnjmsilva152@gmail.com>
2026-03-26 23:24:46 +0530

    Day 12: Updated

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom";
 import AppShell from "../shared/components/AppShell";
+import ErrorBoundary from "../shared/components/ErrorBoundary";
+import { TelemetryProvider } from "../shared/telemetry/TelemetryProvider";
 
 export default function App() {
   return (
-    <AppShell>
-      <Outlet />
-    </AppShell>
+    <TelemetryProvider>
+      <AppShell>
+        <ErrorBoundary>
+          <Outlet />
+        </ErrorBoundary>
+      </AppShell>
+    </TelemetryProvider>
   );
 }
