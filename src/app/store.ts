11f08f5d5791077ff60cc4f6c918079615c6cd1f
Isanjalee <ihnjmsilva152@gmail.com>
2026-03-27 21:43:36 +0530

    Day 13: Updates on Store

--- src/app/store.ts
+++ src/app/store.ts
@@ -17,7 +17,7 @@ export function createAppStore(preloadedState?: Partial<RootState>) {
       getDefaultMiddleware().concat(tasksApi.middleware),
     devTools: import.meta.env.DEV
       ? {
-          name: "React Redux Refresh - Day 12",
+          name: "React Redux Refresh - Day 13",
           trace: true,
           traceLimit: 25,
         }
